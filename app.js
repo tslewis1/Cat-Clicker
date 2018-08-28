d8e24567e1e96dfa90693022b661f5b411ef6c92
Taryn Lewis <tarynslewis@gmail.com>
2018-08-28 12:36:47 -0700

    Create view for the cat names/navigation list

--- app.js
+++ app.js
@@ -36,13 +36,47 @@ $(function() {
 
 	var navListView = {
 
+		// Initializes the cat name/navigation list view 
+		init: function() {
+			// the nav-list-view div
+			this.$navListView = $('#nav-list-view');
+			// the cat image list items
+			this.catImageTemplate = $('script[data-template="cat-display-area"]').html();
+			this.catNavList = $('<ul id="cat-nav-list"></ul>');
+
+			this.$navListView.click('a', function() {
+				let cat = $(this).parents().data();
+				octopus.selectedCat(cat);
+			});
+
+			this.render();
+		},
+
+		// Render function visualizes the cat name/navigation list view
+		render: function() {
+			let $navListView = this.$navListView,
+				catImageTemplate = this.catImageTemplate,
+				catNavList = this.catNavList;
+
+			octopus.getAllCats().forEach(function(cat) {
+				let currentTemplate = catImageTemplate
+				.replace(/{{id}}/, cat.id)
+				.replace(/{{name}}/, cat.name);
+				catNavList.append(currentTemplate);
+				return false;
+			});
+
+			$navListView.html(catNavList);
+		}
 	};
 
 	var displayView = {
-
+		
 	};
 
 	var adminFormView = {
 
 	};
+
+	// octopus.init();
 });
